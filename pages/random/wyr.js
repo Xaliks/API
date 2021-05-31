9b17571a53d826dc904f32c850851edcf0cc7104
Xaliks <miskinmatvej77@gmail.com>
2021-05-31 20:26:38 +0300

    Новое: Ещё больше информации в WYR

--- pages/random/wyr.js
+++ pages/random/wyr.js
@@ -6,16 +6,27 @@ module.exports = {
     const data = await fetch("http://either.io").then((resp) => resp.text());
     const $ = cheerio.load(data);
 
+    const title = $('h2[id="question-title"]').text();
     const wyr = $('div[class="option"] a').text().split(" \n");
-    const votes = $('span[class="contents"]').text().split(" votes")[0];
+    const votes = [
+      $(
+        'div[class="result result-1"] div[class="total-votes"] span[class="count"]'
+      ).html(),
+      $(
+        'div[class="result result-2"] div[class="total-votes"] span[class="count"]'
+      ).html(),
+    ];
+    const description = $('p[class="more-info"]').text() || null;
+    const author = $('span[id="question-author"] a').text();
     const tag = $('ul[class="tags"] li a').text();
-    const author = $('span[id="question-author"] a').text()
 
     return {
+      title,
       questions: [wyr[1], wyr[2]],
       votes,
+      description,
+      author,
       tag,
-      author
     };
   },
 };
