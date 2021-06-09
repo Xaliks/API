8d1d887a87352e2ce5de12ac05bf91147eca799e
Xaliks <miskinmatvej77@gmail.com>
2021-06-09 15:51:23 +0300

    :hammer: Другое: Другой формат вопросов (WYR). Добавлены проценты

--- pages/random/wyr.js
+++ pages/random/wyr.js
@@ -10,22 +10,46 @@ module.exports = {
     const title = $('h2[id="question-title"]').text();
     const wyr = $('div[class="option"] a').text().split(" \n");
     const votes = [
-      $(
-        'div[class="result result-1"] div[class="total-votes"] span[class="count"]'
-      ).html(),
-      $(
-        'div[class="result result-2"] div[class="total-votes"] span[class="count"]'
-      ).html(),
+      Number(
+        $(
+          'div[class="result result-1"] div[class="total-votes"] span[class="count"]'
+        )
+          .html()
+          .replace(/,+/g, "")
+      ),
+      Number(
+        $(
+          'div[class="result result-2"] div[class="total-votes"] span[class="count"]'
+        )
+          .html()
+          .replace(/,+/g, "")
+      ),
     ];
-    const total_votes = $('span[class="contents"]').text().split(" votes")[0];
+    const total_votes = Number(
+      $('span[class="contents"]').text().split(" votes")[0].replace(/,+/g, "")
+    );
+    const percentage = [
+      ((votes[0] / total_votes) * 100).toFixed(2),
+      ((votes[1] / total_votes) * 100).toFixed(2),
+    ];
     const description = $('p[class="more-info"]').text() || null;
     const author = $('span[id="question-author"] a').text();
     const tag = $('ul[class="tags"] li a').text();
 
     return {
       title,
-      questions: [wyr[1], wyr[2]],
-      votes,
+      questions: [
+        {
+          question: wyr[1],
+          votes: votes[0],
+          percentage: percentage[0],
+        },
+        {
+          question: wyr[2],
+          votes: votes[1],
+          percentage: percentage[1],
+        },
+      ],
       total_votes,
       description,
       author,
