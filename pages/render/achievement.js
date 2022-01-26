2897119219b0c3ffdbba08f3a3591566d9cae9bb
Xaliks <miskinmatvej77@gmail.com>
2022-01-26 22:19:16 +0300

    Оптимизация (наверное)

--- pages/render/achievement.js
+++ pages/render/achievement.js
@@ -21,39 +21,38 @@ module.exports = (app) => {
     if (isNaN(icon) || icon < 1 || icon > icons.length)
       return utils.error(resp, `Параметр 'image' должен быть числом от 1 до ${icons.length}!`, usage, examples);
 
-    const tmp = createCanvas(0, 64).getContext("2d");
-    registerFont(path.join(__dirname, "../..", "./assets/fonts/Minecraft.ttf"), { family: "Minecraft" });
-    tmp.font = "15px Minecraft";
-
-    const textWidth = tmp.measureText(text).width + 100;
-    const titleWidth = tmp.measureText(title).width + 100;
-    const x = titleWidth > textWidth ? titleWidth : textWidth;
-
-    const canvas = createCanvas(x, 64);
-    const ctx = canvas.getContext("2d");
-
     const front = await loadImage(path.join(__dirname, "../..", `./assets/images/achievement/${icon}.png`));
+    const end = await loadImage(path.join(__dirname, "../..", "./assets/images/achievement/end.png"));
+
+    const canvas = createCanvas(0, 64);
+    const ctx = canvas.getContext("2d");
+  
+    registerFont(path.join(__dirname, "../..", "./assets/fonts/Minecraft.ttf"), { family: "Minecraft" });
+    ctx.font = "15px Minecraft";
+  
+    const textWidth = ctx.measureText(text).width;
+    const titleWidth = ctx.measureText(title).width;
+    canvas.width = 8 + front.width + end.width + (titleWidth > textWidth ? titleWidth : textWidth);
+
     ctx.drawImage(front, 0, 0);
+    ctx.drawImage(end, canvas.width - end.width, 0);
 
-    const end = await loadImage(path.join(__dirname, "../..", "./assets/images/achievement/achend.png"));
-    ctx.drawImage(end, x - 20, 0);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(front.width, 0, canvas.width - end.width - front.width, 64);
 
-    const mid = await loadImage(path.join(__dirname, "../..", "./assets/images/achievement/achmid.png"));
+    ctx.fillStyle = "#555555";
+    ctx.fillRect(front.width, 2, canvas.width - end.width - front.width, 60);
 
-    // BG
-    let i = 60;
-    while (i <= x - 20) {
-      ctx.drawImage(mid, i, 0);
-      ++i;
-    }
+    ctx.fillStyle = "#212121";
+    ctx.fillRect(front.width, 6, canvas.width - end.width - front.width, 52);
 
     ctx.font = "15px Minecraft";
 
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillText(text, 60, 49);
+    ctx.fillText(text, 60, 48);
 
     ctx.fillStyle = "#FFFF00";
-    ctx.fillText(title, 60, 30);
+    ctx.fillText(title, 60, 29);
 
     resp.setHeader("Content-Type", "image/png");
     return canvas.pngStream().pipe(resp);
